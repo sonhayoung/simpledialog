28b48b990cfae6d18356f724eb72f6b7c1d3d610
HaYoung Son <thsgkdud123@naver.com>
2016-06-07 18:56:47 +0900

    first commit

---
+++ app/src/main/java/kr/hs/emirim/thsgkdud123/simpledialog/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.thsgkdud123.simpledialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstancgiteState);
+        setContentView(R.layout.activity_main);
+    }
+}
