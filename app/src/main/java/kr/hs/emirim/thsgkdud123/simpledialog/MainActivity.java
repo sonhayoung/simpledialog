faa74202cfd4eee5063f4bf27a6e32df6b9d2cfc
HaYoung Son <thsgkdud123@naver.com>
2016-06-09 19:42:51 +0900

    #Change CheckBox

--- app/src/main/java/kr/hs/emirim/thsgkdud123/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/thsgkdud123/simpledialog/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     String[] items={"젤리빈","킷캣","롤리팝"};
-    boolean[] checkArr={false,true};
+    boolean[] checkArr={false,true,false};
     Button but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +32,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });*/
 
-        dialog.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+//        dialog.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                but.setText(items[which]);
+//            }
+//        });
+        dialog.setMultiChoiceItems(items, checkArr, new DialogInterface.OnMultiChoiceClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                but.setText(items[which]);
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                     but.setText(items[which]);
             }
         });
         dialog.setIcon(R.drawable.first_icon);
